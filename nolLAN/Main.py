a2c221a76a5db3e594fed77af35eee4cf6fdafb2
Tejp <tejpbit@hotmail.se>
2014-09-06 02:47:12 +0200

    fixed bug with row interference amongst themselfs... when row got removed. indexes was changed.

--- nolLAN/Main.py
+++ nolLAN/Main.py
@@ -28,11 +28,16 @@ def print_text_slow(string):
 
     row_list = string.split('\n')
 
-    string_to_list = lambda s : list(s)
+    #convert all rows from strings to lists
     for row in row_list:
-        row_list[row_list.index(row)] = string_to_list(row)
+        row_list[row_list.index(row)] = list(row)
 
-    # row_list =
+    # add the index of the row to the first index of the row, it'll be useful later i promise
+    for i in range(0, len( row_list)):
+        row_list[i].insert(0, i)
+
+    #remove last row of list. cus it's empty for some reason
+    del row_list[-1]
 
     #list to remember how many chars has been printed in each row
     marker_list = []
@@ -41,18 +46,18 @@ def print_text_slow(string):
 
     while (row_list):
         rand_row = random.choice(row_list)
-        row_list_index = row_list.index(rand_row)
-        if (len(rand_row) != 0):
-            sleep(0.001)
-            marker_list[row_list_index] += 1
-            display_char_at_pos(rand_row.pop(0), (marker_list[row_list_index]*10, row_list.index(rand_row)*10))
-        else:
+        #sleep(0.001)
+        marker_list[rand_row[0]] += 1
+        display_char_at_pos(rand_row.pop(1), (marker_list[rand_row[0]]*10, rand_row[0]*10))
+        if len(rand_row) <= 1:
+            print "removing row %s" % rand_row
             row_list.remove(rand_row)
 
 def display_char_at_pos(char, (x,y)):
     text = monospace10.render(char, 1, TURQUOISE)
     textpos = text.get_rect()
     textpos.x=x
+    #print "y: %s" % y
     textpos.y=y
     background.blit(text, textpos)
     screen.blit(background, (0, 0))
