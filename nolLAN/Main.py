f44b7016265dfe2c624d612c058680353df6f680
Tejp <tejpbit@hotmail.se>
2014-09-06 23:47:43 +0200

    fade-in fixed.

--- nolLAN/Main.py
+++ nolLAN/Main.py
@@ -24,9 +24,13 @@ background.fill(BLACK)
 
 digitLogo = Pictoasciiart.get_ascii_art("digit.png")
 
-def print_text_slow(string):
+def print_text_slow(row_list, (x, y)):
+    """render the provided string one char at a time at specified position (x,y) is the middle of the text"""
 
-    row_list = string.split('\n')
+    # calculation to render asciiart in center
+    font_size = (10, 10)
+
+    row_list = list(row_list)
 
     #convert all rows from strings to lists
     for row in row_list:
@@ -36,7 +40,7 @@ def print_text_slow(string):
     for i in range(0, len( row_list)):
         row_list[i].insert(0, i)
 
-    #remove last row of list. cus it's empty for some reason
+    #remove last row of list. cus it's empty for some reason, probably beacause original string ended with '\n'
     del row_list[-1]
 
     #list to remember how many chars has been printed in each row
@@ -44,16 +48,13 @@ def print_text_slow(string):
     for i in row_list:
         marker_list.append(0)
 
-    font_size = 8
-    ascii_art_size = (font_size * (len(row_list[0])-2), font_size * len(row_list))
-    screen_size = screen.get_size()
-    offset_to_centralize_ascii_art = (screen_size[0]/2 - ascii_art_size[0]/2, screen_size[-1]/2 - ascii_art_size[-1]/2)
-
     while (row_list):
         rand_row = random.choice(row_list)
         #sleep(0.001)
         marker_list[rand_row[0]] += 1
-        display_char_at_pos(rand_row.pop(1), (offset_to_centralize_ascii_art[0] + marker_list[rand_row[0]]*font_size, offset_to_centralize_ascii_art[-1] + rand_row[0]*font_size)) #TODO magic numbers (font_size). size should be gotten from the font beeing used no?
+        x_pos = x + marker_list[rand_row[0]]*font_size[0] - 10 # (-10) TODO offbyone error corrected here. Why idunno?
+        y_pos = y + rand_row[0]*font_size[-1]
+        display_char_at_pos(rand_row.pop(1), (x_pos, y_pos)) #TODO magic numbers (font_size). size should be gotten from the font beeing used no?
         if len(rand_row) <= 1:
             row_list.remove(rand_row)
 
@@ -82,19 +83,35 @@ def fade_in(pic, pos=(0,0), adddelay=0):
     sleep(1)
 
 def get_picsize_from_asciipic(asciipic, font=monospace8):
-    size = font.size(''.join(asciipic[0]))
-    height = len(asciipic) * size[1]
-    return (size[0], height)
+
+    width = len(asciipic[0]) * 10
+    height = len(asciipic) * 10
+    return (width, height)
 
 def main():
+    digitAscii = Pictoasciiart.get_ascii_art("digit2.jpg").split('\n')
 
-    digitAscii = Pictoasciiart.get_ascii_art("digit.png")
-    print_text_slow(digitAscii)
-    digitPic = pygame.transform.scale(pygame.image.load('digit.png'), (get_picsize_from_asciipic(digitAscii))).convert()
+    # calculation to render asciiart in center
+    screen_size = screen.get_size()
+    center_screen = (screen_size[0]/2, screen_size[-1]/2)
+    picsize = get_picsize_from_asciipic(digitAscii)
+    render_pos_to_match_center = (center_screen[0] - picsize[0]/2, center_screen[-1] - picsize[-1]/2)
+
+    del digitAscii[-1]
+    print_text_slow(digitAscii, render_pos_to_match_center)
+    digitPic = pygame.transform.scale(pygame.image.load('digit2.jpg'), (get_picsize_from_asciipic(digitAscii))).convert()
 
     #sleep(1)
     #fade_to_black()
-    fade_in(digitPic)
+    #fade_in(digitPic)
+
+    digit_pic_rect = digitPic.get_rect()
+    print digit_pic_rect
+    fade_in(digitPic, render_pos_to_match_center, 0.03)
+
+    #background.blit(digitPic, digit_pic_rect)
+    #screen.blit(background, (0, 0))
+    #pygame.display.update(digit_pic_rect)
 
     while True:
         for event in pygame.event.get():
